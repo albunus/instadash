c69e210fdaf0dead187ddaa46834b59182e4d732
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-02 18:42:25 +0300

    added image class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+
+class Images(models.Model):
+    name = models.CharField(max_length=100)
+    caption = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image')
+    
+    def __str__(self):
+        return self.pic_name
